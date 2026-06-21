388059d1c73117dccb3b81dd25bd5bba1f7413f8
Amina Antari <aantari@c4r6p7.1337.ma>
2026-06-21 06:58:14 +0100

    fly_in

--- fly/parser.py
+++ fly/parser.py
@@ -1,37 +1,141 @@
-from pyparsing import (Suppress, one_of, alphanums, Optional, ParseException,
-                       Word, printables, nums, ZeroOrMore)
+from pyparsing import (
+    Word, alphas, nums, Optional,
+    Suppress, Group, OneOrMore, ParseException,
+    one_of, pythonStyleComment
+)
+import pyparsing
 
-# tokens
-NAME = Word(printables, exclude_chars=" -")
+
+class Connection:
+    def __init__(self, hub1, hub2, max_connections=1) -> None:
+        self.hub1 = hub1
+        self.hub2 = hub2
+        self.max_connections = max_connections
+
+    def __eq__(self, value):
+        if not isinstance(value, Connection):
+            return NotImplemented
+        return (
+            (self.hub1 == value.hub2 and self.hub2 == value.hub1)
+            or
+            (self.hub1 == value.hub1 and self.hub2 == value.hub2)
+        )
+
+    def __hash__(self):
+        return hash(frozenset([self.hub1, self.hub2]))
+
+    def __str__(self):
+        return f"{self.hub1}-{self.hub2}"
+
+
+class Hub:
+    def __init__(self, name, x, y):
+        self.name = name
+        self.x = x
+        self.y = y
+
+    def __eq__(self, value):
+        return (
+            self.name == value.name
+            or
+            (self.x == value.x and self.y == value.y)
+        )
+
+    def __hash__(self):
+        return hash((self.name, self.x, self.y))
+
+
+connections = set()
+hubs = set()
+
+
+def save_hub(text, loc, tokens):
+    hub_info = tokens[0]
+
+    name = hub_info["name"]
+    x = int(hub_info["x"])
+    y = int(hub_info["y"])
+
+    hub = Hub(name, x, y)
+
+    if hub in hubs:
+        raise pyparsing.ParseFatalException(
+            text,
+            loc,
+            f"Error: repeated hub '{name}' ({x}, {y})!"
+        )
+
+    hubs.add(hub)
+
+
+def save_connection(text, loc, tokens):
+    connection_info = tokens[0]
+
+    hub1 = connection_info["from_zone"]
+    hub2 = connection_info["to_zone"]
+
+    hubs_name = [hub.name for hub in hubs]
+
+    connection = Connection(hub1, hub2)
+
+    if connection in connections:
+        raise pyparsing.ParseFatalException(
+            text,
+            loc,
+            f"Error: repeated connection '{hub1}-{hub2}'!"
+        )
+
+    if hub1 not in hubs_name:
+        raise pyparsing.ParseFatalException(
+            text,
+            loc,
+            f"Error: hub '{hub1}' not declared!"
+        )
+
+    if hub2 not in hubs_name:
+        raise pyparsing.ParseFatalException(
+            text,
+            loc,
+            f"Error: hub '{hub2}' not declared!"
+        )
+
+    connections.add(connection)
+
+
+# TOKENS
+NAME = Word(pyparsing.printables, exclude_chars=" -")
 INTEGER = Word(nums)
 
-# nb drones
+# NB_DRONES
 NB_DRONES = (Suppress("nb_drones") + Suppress(":") + INTEGER("count"))
 
 # HUB BASIC
 HUB_BASIC = (NAME("name") + INTEGER("x") + INTEGER("y"))
-# ZONE METADATA
+
+# METADATA
 ZONE = (Suppress("zone") + Suppress("=") +
         one_of("restricted normal blocked priority")("zone"))
-# COLOR METADATA
-COLOR = (Suppress("color") + Suppress("=") + Word(alphanums)("color"))
-# MAX_DRONES
+
+COLOR = (Suppress("color") + Suppress("=") + Word(alphas)("color"))
+
 MAX_DRONES = (Suppress("max_drones") + Suppress("=") + INTEGER("max_drones"))
 
-# ALL META_DATA
-METADATA = (Suppress("[") + ZeroOrMore(ZONE | COLOR | MAX_DRONES)
-            + Suppress("]"))
+METADATA = (Suppress("[") + (Optional(ZONE) & Optional(COLOR) &
+                             Optional(MAX_DRONES)) + Suppress("]"))
 
-# HUB COMPLETE
-HUB = (Suppress("hub") + Suppress(":") + HUB_BASIC + Optional(METADATA))
+# HUB
+HUB = Group(Suppress("hub") + Suppress(":") + HUB_BASIC + Optional(METADATA))
+HUB.set_parse_action(save_hub)
 
 # START_HUB
-START_HUB = (Suppress("start_hub") + Suppress(":") + HUB_BASIC
-             + Optional(METADATA))
+START_HUB = Group(Suppress("start_hub") + Suppress(":")
+                  + HUB_BASIC + Optional(METADATA))("start_hub")
+START_HUB.set_parse_action(save_hub)
 
 # END_HUB
-END_HUB = (Suppress("end_hub") + Suppress(":") + HUB_BASIC
-           + Optional(METADATA))
+END_HUB = Group(Suppress("end_hub") + Suppress(":") + HUB_BASIC
+                + Optional(METADATA))("end_hub")
+END_HUB.set_parse_action(save_hub)
 
 # CONNECTION METADATA
 MAX_LINK_CAPACITY = (Suppress("max_link_capacity") + Suppress("=")
@@ -39,88 +143,28 @@ MAX_LINK_CAPACITY = (Suppress("max_link_capacity") + Suppress("=")
 
 CONNECTION_METADATA = (Suppress("[") + MAX_LINK_CAPACITY + Suppress("]"))
 
-# CONNECTION COMPLETE
-CONNECTION = (Suppress("connection") + Suppress(":") + NAME("from_zone") +
-              Suppress("-") + NAME("to_zone") + Optional(CONNECTION_METADATA))
+# CONNECTION
+CONNECTION = Group(Suppress("connection") + Suppress(":") + NAME("from_zone")
+                   + Suppress("-") + NAME("to_zone")
+                   + Optional(CONNECTION_METADATA))
+CONNECTION.set_parse_action(save_connection)
 
-# STORAGE
-hubs = []
-connections = []
-start_hub = None
-end_hub = None
-nb_drones = None
-# PARSING
-with open("map.txt") as f:
-    for line in f:
+# GLOBAL RULES
+rules = (NB_DRONES - (END_HUB & START_HUB &
+                      Group(OneOrMore(HUB))("liste_hubs") &
+                      Group(OneOrMore(CONNECTION))("liste_connections")))
 
-        line = line.split("#")[0].strip()
-        if not line:
-            continue
-
-        try:
-            if line.startswith("nb_drones"):
-                nb_drones = NB_DRONES.parse_string(line)["count"]
-
-            elif line.startswith("start_hub"):
-                start_hub = START_HUB.parse_string(line)
-
-            elif line.startswith("end_hub"):
-                end_hub = END_HUB.parse_string(line)
-
-            elif line.startswith("hub"):
-                hubs.append(HUB.parse_string(line))
-
-            elif line.startswith("connection"):
-                connections.append(CONNECTION.parse_string(line))
-
-            else:
-                raise ValueError("Unknown line")
-
-        except ParseException as e:
-            print("ERROR in line:", line)
-            print(e)
-            exit(1)
-
+rules.ignore(pythonStyleComment)
 
 try:
-    if start_hub is None:
-        raise ValueError("Missing required field: start_hub")
+    import sys
 
-    if end_hub is None:
-        raise ValueError("Missing required field: end_hub")
+    result = rules.parse_file(sys.argv[1], parse_all=True)
 
-    if nb_drones is None:
-        raise ValueError("Missing required field: nb_drones")
-except Exception as e:
-    print(e)
-    
+    for key, value in result.asDict().items():
+        print(f"{key}: {value}")
 
+    print(*connections)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+except (ParseException, pyparsing.exceptions.ParseBaseException) as e:
+    print(e.explain())
