2489618f40de5015e5eefe376d2dec3b3ecd2b7b
aantari-00 <codejinn.0@gmail.com>
2026-07-15 19:06:49 +0100

    v3

--- fly/Router.py
+++ fly/Router.py
@@ -7,33 +7,55 @@ class Router:
         self.graph = graph
         self.dijkstra = Dijkstra(graph)
 
-    def find_paths(self, max_path=10):
-        first_path = self.dijkstra.get_path()
-        if first_path is None:
-            return []
+def find_paths(self, max_path=10):
+    first = self.dijkstra.get_path()
+    if first is None:
+        return []
 
-        accepted = [first_path]
+    accepted = [first]
+
+    while len(accepted) < max_path:
         candidates = []
 
-        while len(accepted) < max_path:
-            previous_path, _ = accepted[-1]
-            for i in range(len(previous_path) - 1):
-                spur_node = previous_path[i]
-                root_path = previous_path[:i + 1]
+        previous_path, _ = accepted[-1]
 
-                hub1 = previous_path[i]
-                hub2 = previous_path[i + 1]
-                removed = self.graph.remove_edge(hub1, hub2)
+        for i in range(len(previous_path) - 1):
 
-                new_path = self.dijkstra.get_path(source=spur_node)
-                self.graph.restore_edge(removed)
-                if new_path is not None:
-                    if new_path not in accepted and new_path not in candidates:
-                        candidates.append(new_path)
-            if not candidates:
-                break
-            candidates.sort(key=lambda x: x[1])
-            accepted.append(candidates.pop(0))
+            spur_node = previous_path[i]
+            root_path = previous_path[:i + 1]
+
+            removed_edges = self.block_known_edges(accepted, root_path, i)
+            removed_nodes = self.block_root_nodes(root_path)
+
+            spur = self.dijkstra.get_path(source=spur_node)
+
+            self.restore_root_nodes(removed_nodes)
+            self.restore_known_edges(removed_edges)
+
+            if spur is None:
+                continue
+
+            spur_path, spur_cost = spur
+
+            total_path = root_path[:-1] + spur_path
+
+            total_cost = len(total_path) - 1
+
+            self.add_candidate(
+                candidates,
+                accepted,
+                total_path,
+                total_cost
+            )
+
+        if not candidates:
+            break
+
+        candidates.sort(key=lambda x: x[1])
+
+        accepted.append(candidates.pop(0))
+
+    return accepted
 
     def block_known_edges(self, accepted, root_path, spur_index):
 
@@ -63,8 +85,19 @@ class Router:
         return removed_nodes
 
     def restore_root_nodes(self, removed_nodes):
-        for removed in reversed(removed_nodes):
+        for removed in removed_nodes:
             self.graph.restore_node(removed_nodes)
 
     def add_candidate(self, candidates, accepted, path, cost):
-        pass
+        if path is None:
+            return
+    
+        for accepted_path, _ in accepted:
+            if accepted_path == path:
+                return
+    
+        for candidate_path, _ in candidates:
+            if candidate_path == path:
+                return
+    
+        candidates.append((path, cost))
